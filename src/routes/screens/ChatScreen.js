6c6f2acc3134bea28e9a4ad2a8f0d807f5be3d0f
bogdanmandic <bogdan.mandic@cadev.rs>
2017-11-17 10:39:51 +0100

    Mob App Stage 1

--- src/routes/screens/ChatScreen.js
+++ src/routes/screens/ChatScreen.js
@@ -1,14 +1,27 @@
-import React, {Component} from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Button, TextInput } from 'react-native';
 import Input from '../../components/Input';
+
 
 
 export default class ChatScreen extends Component {
     state = { msg: '' }
 
-    static navigationOptions = ({ navigation }) => ({
-        title: `Chat with ${navigation.state.params.user}`,
-    });
+    static navigationOptions = ({ navigation }) => {
+        const { state, setParams } = navigation;
+        const isInfo = state.params.mode === 'info';
+        const { user } = state.params;
+        return {
+            title: isInfo ? `${user}'s Contact Info` : `Chat with ${state.params.user}`,
+            headerRight: (
+                <Button
+                    title={isInfo ? 'Done' : `${user}'s info`}
+                    onPress={() => setParams({ mode: isInfo ? 'none' : 'info' })}
+                />
+            ),
+        };
+    };
+
     render() {
         const { navigate } = this.props.navigation;
         const { params } = this.props.navigation.state;
@@ -19,13 +32,13 @@ export default class ChatScreen extends Component {
                     onPress={() => navigate('Tab')}
                     title='Go to Tab view chat'
                 />
-                <Input 
-                    placeholder="Say hello"
-                    label="Send message"
-                    value={this.state.msg} 
-                    onChangeText={msg => this.setState({ msg })}
+                <TextInput
+                    style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                    placeholder='Send message'
+                    onChangeText={(msg) => this.setState({ msg })}
+                    value={this.state.msg}
                 />
             </View>
-        ); 
+        );
     }
 }
